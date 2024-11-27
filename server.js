7b3be8a9a89a52a4199f39881174ac4acbc4f1a5
masood <masoodacheampong@gmail.com>
2024-11-27 09:58:52 +0000

    Basic Routing

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
   // Set header content type
   res.setHeader("Content-Type", "text/html");
-  res.write("<head><link rel='stylesheet' href='#'></head>");
-  res.write("<h1>Hello, World!</h1>");
-  res.write("<p>Hello again Ninjas</p>");
-  res.end();
+  // Main Path
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+  // Send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
