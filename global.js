3645fe13f15113fcdfae2c554fe67b35454dcbf1
masood <masoodacheampong@gmail.com>
2024-11-27 08:42:55 +0000

    learning about the Global Object's influence in Node

---
+++ global.js
@@ -0,0 +1,17 @@
+// Global Object
+// console.log(global);
+
+// Accessing the global object's methods
+// setTimeout(() => {
+//   console.log("In the timeout");
+//   clearInterval(int);
+// }, 3000);
+
+// SetInterval
+// const int = setInterval(() => {
+//   console.log("In the interval");
+// }, 1000);
+
+// Accessing the directory name and the full file path name
+// console.log(__dirname);
+// console.log(__filename);
