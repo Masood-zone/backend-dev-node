1e0e4489805b6cc596b7811a11a813e6a465fe40
masood <masoodacheampong@gmail.com>
2024-11-27 09:53:50 +0000

    Response object, writing on the response

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  // Set header content type
+  res.setHeader("Content-Type", "text/html");
+  res.write("<head><link rel='stylesheet' href='#'></head>");
+  res.write("<h1>Hello, World!</h1>");
+  res.write("<p>Hello again Ninjas</p>");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
